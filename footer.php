af863db05a26cc7a72e91fecdd415a796e883b0e
Heiko <heiko@HeikosL15.fritz.box>
2023-11-12 16:49:05 +0100

    Added link

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 echo("
 	<hr>
 	<font size=\"1\">
-		<center>$gSiteName</center>
+		<center>$gSiteName - <a href=\"https://github.com/WroDo/pdfconvertomat\">Project Page</a></center>
 	</font>
 	</body>
 </html>
